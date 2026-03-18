8fd24a00a69ddfa246a4993b65487af4da9185b0
Talysson Vasconcelos <talyssonpestinha.tp@gmail.com>
2026-03-18 14:37:39 -0300

    feat: adicionar função atualizarPatrimonio no service

--- Codigo/lib/services/PatrimonioInventariado_service.dart
+++ Codigo/lib/services/PatrimonioInventariado_service.dart
@@ -21,10 +21,15 @@
       return result.map((item) => PatrimonioInventariado.fromMap(item)).toList();
     }
 
-    Future<int> excluirPatrimonio(int id) async {
-      if(id == Null){
-        throw Exception("exclusão falhou, id não identificado");
-      }
-      return await databaseHelper.deletePatrimonio(id);
-   }
- }
+
+Future<int> atualizarPatrimonio(PatrimonioInventariado ativo) async {
+    return await databaseHelper.updatePatrimonio(ativo.toMap());
+  }
+
+  Future<int> excluirPatrimonio(int id) async {
+    if(id == null){
+      throw Exception("exclusão falhou, id não identificado");
+    }
+    return await databaseHelper.deletePatrimonio(id);
+  }
+}
